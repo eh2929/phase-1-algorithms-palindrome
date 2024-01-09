ae1729e8e6da276b1fb2ec8595cb1b8fa393a3a0
Ethan <ethanhough29@gmail.com>
2024-01-09 17:07:00 -0500

    complete lab

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ function isPalindrome(word) {
 }
 
 /* 
-  Add your pseudocode here
+  Create a function that takes in a string and returns true if it's a palindrome and false if it isn't. A palindrome is a word that is spelled the same way forwards and backwards.
 */
 
 /*
-  Add written explanation of your solution here
+  This for loop reverses the given word by starting from the last character's index and iterating + appending each character to the reversed string. 
 */
+
+function isPalindrome(word) {
+  let reversed = "";
+
+  for (let currentIndex = word.length - 1; currentIndex >= 0; currentIndex--) {
+    reversed += word[currentIndex];
+  }
+
+  return reversed === word;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
